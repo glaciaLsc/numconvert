bcad3e68e6790d0c695c228eb533b37bcd51bab1
gjustice <garrett.justice50@gmail.com>
2020-06-11 15:05:03 -0400

    Program functionality is under way!

--- src/numconvert.c
+++ src/numconvert.c
@@ -13,61 +13,29 @@ typedef enum
 	F = 15,
 } digit;
 
-/* Linked list data structure */
-struct node
+/* TODO: Implement parsing for characters A-F, which will
+ * correspond with digit values from 10-11.
+ */
+void convertfromdecimal(int fromvalue, char **tovalue, unsigned short base)
 {
-	char data;
-	struct node* next;
-	struct node* prev;
-};
+	int temp = fromvalue;
+	char newvalue[18];
 
-void append(struct node** headref, char value)
-{
-	struct node* newnode = (struct node *)malloc(sizeof(struct node));
-	struct node* current = *headref;
-
-	newnode->data = value;
-	newnode->next = NULL;
-	newnode->prev = NULL;
-
-	if (*headref == NULL)
+	do
 	{
-		*headref = newnode;
-	}
-	else
-	{
-		while (current->next != NULL)
-			current = current->next;
-		
-		newnode->prev = current;
-		current->next = newnode;
-	}
+		// Store remainder value in temporary string
+		char remainder[2];
+		sprintf(remainder, "%d", (temp % base));
+		// Append remainder value to newvalue string		
+		strcat(newvalue, remainder);
+		temp /= base;
+	} while (temp != 0);
+
+	printf("%s\n", newvalue);
 }
 
-char* convertfromdecimal(char* n)
+void converttodecimal(char *fromvalue, char **tovalue, unsigned short base)
 {
-	return n;
-}
-
-char* converttodecimal(char* n)
-{
-	return n;
-}
-
-char* convert(char* n)
-{
-	return n;
-}
-
-void print(struct node* headref)
-{
-	while (headref->next != NULL)
-		headref = headref->next;
-	while (headref != NULL)
-	{
-		printf("%c\n", headref->data);
-		headref = headref->prev;
-	}
 }
 
 void printhelpdisplay()
@@ -116,7 +84,8 @@ int flagcheck(int index, char* argv[])
 }
 
 /* Check user arguments */
-void checkargs(unsigned short *flags, int argc, char* argv[])
+void checkargs(unsigned short *flags, char **fromvalue, int
+		argc, char* argv[])
 {
 	if (argc < 3)
 	{
@@ -131,6 +100,7 @@ void checkargs(unsigned short *flags, int argc, char* argv[])
 		{
 			flags[0] = 10;
 			flags[1] = flagcheck(2, argv);
+			*fromvalue = argv[1];
 		}
 		// If 1st argument is flag and 2nd argument is not
 		// flag, default "to" flag to 10
@@ -138,6 +108,7 @@ void checkargs(unsigned short *flags, int argc, char* argv[])
 		{
 			flags[1] = 10;
 			flags[0] = flagcheck(1, argv);
+			*fromvalue = argv[2];
 		}
 		else
 		{
@@ -149,6 +120,7 @@ void checkargs(unsigned short *flags, int argc, char* argv[])
 	{
 		flags[0] = flagcheck(1, argv);
 		flags[1] = flagcheck(3, argv);
+		*fromvalue = argv[2];
 	}
 	else
 	{
@@ -160,22 +132,36 @@ void checkargs(unsigned short *flags, int argc, char* argv[])
 /* TODO: Implement actual program functionality. Can expect
  * difficulties when attempting to parse letters in >b10
  * number systems.
- *
- * Linked list functionality is complete-- now it's time for
- * arithmetic functions.
- *
- * Functions need to include flags in paramaters, as well as
- * headref
  */
 int main(int argc, char* argv[])
 {
-	// Array to store flag values
-	unsigned short flags[2];
-	// Check arguments & set flag values
-	checkargs(flags, argc, argv);
+	// Array to store flag values-- the first index is the
+	// base of the number to be converted; the second index
+	// is the base of the converted number; the third index
+	// is the value of the number to be converted
+	unsigned short flags[3];
+	// Strings to store values of original and converted
+	// numbers
+	char *fromvalue;
+	char *tovalue;
+	// Check arguments & set flag / number values
+	checkargs(flags, &fromvalue, argc, argv);
 
 	// Run program according to flag values set by user
-	struct node* head = NULL;
+	if (flags[0] == 10)
+	{
+		convertfromdecimal(atoi(fromvalue), &tovalue,
+				flags[1]);
+	}
+	else if (flags[1] == 10)
+	{
+		converttodecimal(fromvalue, &tovalue, flags[0]);
+	}
+	else
+	{
+		converttodecimal(fromvalue, &tovalue, flags[0]);
+		convertfromdecimal(atoi(tovalue), &tovalue, flags[1]);
+	}
 
 	return 0;
 }
