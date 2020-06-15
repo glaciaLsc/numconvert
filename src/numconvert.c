56d317839f2d675db5e293b6a86a5cd7452c6c41
gjustice <garrett.justice50@gmail.com>
2020-06-15 17:27:24 -0400

    Program appears to be fully functional! Help guide to be written soon

--- src/numconvert.c
+++ src/numconvert.c
@@ -22,9 +22,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-/* Map decimal value to higher-base character value */
-char map(int n)
+/* Map decimal value to higher-base character representation
+ * */
+char maptochar(int n)
 {
 	if (n == 0)
 		return '0';
@@ -60,6 +62,42 @@ char map(int n)
 		return 'F';
 }
 
+int maptoint(char c)
+{
+	if (c == '0')
+		return 0;
+	else if (c == '1')
+		return 1;
+	else if (c == '2')
+		return 2;
+	else if (c == '3')
+		return 3;
+	else if (c == '4')
+		return 4;
+	else if (c == '5')
+		return 5;
+	else if (c == '6')
+		return 6;
+	else if (c == '7')
+		return 7;
+	else if (c == '8')
+		return 8;
+	else if (c == '9')
+		return 9;
+	else if (c == 'A')
+		return 10;
+	else if (c == 'B')
+		return 11;
+	else if (c == 'C')
+		return 12;
+	else if (c == 'D')
+		return 13;
+	else if (c == 'E')
+		return 14;
+	else if (c == 'F')
+		return 15;
+}
+
 /* Replace string with its backwards equivalent */
 void reverse(char *string, int size)
 {
@@ -71,17 +109,15 @@ void reverse(char *string, int size)
 	strcpy(string, temp);
 }
 
-void convertfromdecimal(int fromvalue, char *tovalue, unsigned short base)
+void convertfromdecimal(unsigned long fromvalue, char *tovalue, unsigned short base)
 {
 	do
 	{
-		// Store remainder value in temporary string
-		char remainder;
-
-		// Append value to remainder
-		remainder = map(fromvalue % base);
+		// Store remainder value in temporary char
+		char remainder = maptochar(fromvalue % base);
 		// Append remainder value to toValue string
 		strcat(tovalue, &remainder);
+		// Update fromValue integer
 		fromvalue /= base;
 	} while (fromvalue != 0);
 
@@ -89,11 +125,28 @@ void convertfromdecimal(int fromvalue, char *tovalue, unsigned short base)
 	reverse(tovalue, strlen(tovalue));
 }
 
-/*
- * TODO: Hi! Implement me pls! c:
- */
 void converttodecimal(char *fromvalue, char *tovalue, unsigned short base)
 {
+	unsigned long newvalue = 0;
+	
+	// Increment newValue with value of digit * base^column
+	for (int i=0; i < strlen(fromvalue); i++)
+		newvalue += (maptoint(fromvalue[i]) * pow(base,
+					strlen(fromvalue)-i-1));
+
+	sprintf(tovalue, "%d", newvalue);
+}
+
+unsigned long returndecimal(char *fromvalue, unsigned short base)
+{
+	unsigned long newvalue = 0;
+
+	// Increment newValue with value of digit * base^column
+	for (int i=0; i < strlen(fromvalue); i++)
+		newvalue += (maptoint(fromvalue[i]) * pow(base,
+					strlen(fromvalue)-i-1));
+
+	return newvalue;
 }
 
 void printhelpdisplay()
@@ -204,18 +257,20 @@ int main(int argc, char* argv[])
 	// Run program according to flag values set by user
 	if (flags[0] == 10)
 	{
-		convertfromdecimal(atoi(fromvalue), tovalue,
+		convertfromdecimal(atol(fromvalue), tovalue,
 				flags[1]);
 		printf("%s\n", tovalue);
 	}
 	else if (flags[1] == 10)
 	{
 		converttodecimal(fromvalue, tovalue, flags[0]);
+		printf("%s\n", tovalue);
 	}
 	else
 	{
-		converttodecimal(fromvalue, tovalue, flags[0]);
-		convertfromdecimal(atoi(tovalue), tovalue, flags[1]);
+		long midvalue = returndecimal(fromvalue, flags[0]);
+		convertfromdecimal(midvalue, tovalue, flags[1]);
+		printf("%s\n", tovalue);
 	}
 
 	return 0;
