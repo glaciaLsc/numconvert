9caa623593d75e045d88cbab2872778f5db1dd9b
glaciaLsc <garrett.justice50@gmail.com>
2021-11-12 22:32:37 -0500

    formatting

--- src/numconvert.c
+++ src/numconvert.c
@@ -13,7 +13,6 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see
 <https://www.gnu.org/licenses/> */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
